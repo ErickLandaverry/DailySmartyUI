406df1314e27487caa2a38d9f0467c7d954e352c
landy <landy.erick20@gmail.com>
2018-05-16 17:01:58 -0600

    modified SearchBar Component to take onSubmit function via props

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm} from 'redux-form';
 
 import { withRouter } from 'react-router-dom';
+
 class SearchBar extends Component {
 
     handleFormSubmit = function({query}) {
-        console.log('trying to handle query',query);
-        this.props.history.push('/results');
+        this.props.onSubmit(query);
     }
 
-    renderInput(field){
-        return<input type="text" placeholder="Search DailySmarty" {...field.input} />
+    renderInput(field) {
+        return <input type="text" placeholder="Search DailySmarty" {...field.input} />
     }
 
     render() {
 
-        const {handleSubmit } = this.props;
+        const { handleSubmit } = this.props;
+
         return (
             <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
                 <Field name="query" component={this.renderInput}/>
             </form>
-        );
+        )
     }
 }
 
@@ -28,5 +29,6 @@ SearchBar = reduxForm({
     form: 'searchBar'
 })(SearchBar);
 
-SearchBar= withRouter(SearchBar);
+SearchBar = withRouter(SearchBar);
+
 export default SearchBar;
