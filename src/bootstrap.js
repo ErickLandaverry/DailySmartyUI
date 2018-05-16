1a76259aaff6cad217a96a9f7a903f4bb1fd202b
landy <landy.erick20@gmail.com>
2018-05-16 14:45:23 -0600

    setup results component

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -13,6 +13,7 @@ import './style/main.scss';
 
 
 import Home from './components/home';
+import Results from './components/results';
 
 function main() {
   ReactDOM.render(
@@ -20,7 +21,7 @@ function main() {
       <BrowserRouter>
         <Switch>
           <Route path='/' component={Home}/>
-          <Route path='/results' component={Home}/>
+          <Route path='/results' component={Results}/>
         </Switch>
       </BrowserRouter>
     </Provider>
